c78be438575cd22e4572586ae6a9a97bd3e3941c
nirav99 <niravhgsc@gmail.com>
2016-09-22 13:39:05 -0500

    Modified driver and readme

--- ReadMe.md
+++ ReadMe.md
@@ -9,3 +9,19 @@ The Approach
 It is based on naive bayes classifier. We generate n-grams of size 1, 2 and 3 characters and feed them to the classifier. The language with the maximum probability is considered as the page language.
 
 The prior counts are taken from another github repository [https://github.com/shuyo/language-detection].
+
+Usage
+======
+
+```cmd
+cd jar
+java -jar LanguagePredictor.jar this is a line of text for which we want to analyze the language
+Predicted page langauge = en
+```
+
+You can also add text to a file, say inputfile and call the program as
+
+```cmd
+cd jar
+cat inputfile | xargs java -jar LanguagePredictor.jar
+```
--- src/com/languagedetector/Driver.java
+++ src/com/languagedetector/Driver.java
@@ -43,16 +43,25 @@ public class Driver
   {
     try
     {
-      String input = null;
+      if(args == null || args.length == 0 || (args.length == 1 && args[0].toLowerCase().contains("help")))
+      {
+        System.err.println("Provide text at command line whose language needs to be determined");
+        return;
+      }
+      StringBuilder input = new StringBuilder();
       
+      for(int i = 0; i < args.length; i++)
+        input.append(args[0]).append(" ");
+      
+ /*     
       input = "This is one line of text. It is written in English. Let's see how this evaluates";
       input = "Die USA machen Russland für den Angriff auf den Uno-Hilfskonvoi in Syrien verantwortlich, liefern aber keine Beweise. Moskau weist alle Schuld von sich - und präsentiert ständig neue Versionen des Tathergangs";
       
       input = "Gabriel, marquis de Ralston, vient d'apprendre l'existence de sa sueur bâtarde Juliana, qui arrive tout droit d'Italie. Pour lancer dans le beau monde cette jeune fille aux origines douteuses, il lui faut un chaperon exemplaire. Pourquoi pas lady Calpurnia Hartwell qui est considérée comme un parangon de vertu ? Elle sera garante de la réputation de Juliana. Sauf que Calpurnia est en train de se rendre compte que sa vie l'ennuie profondément. Elle a même établi une liste de choses scandaleuses à faire ";
       input = "Herren damen kinder Bekleidung";
-
+*/
       Driver driver = new Driver();
-      System.out.println("Predicted page langauge = " + driver.getLangauge(input));
+      System.out.println("Predicted page langauge = " + driver.getLangauge(input.toString()));
     }
     catch(Exception e)
     {
